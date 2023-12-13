4ec005411bdaaca3a044c76f0e9a4e6c4cf7e652
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-12-14 02:28:08 +0300

    Welcome Screen

---
+++ app/src/main/java/me/t3sl4/gelkurye/Screens/General/Welcome.java
@@ -0,0 +1,16 @@
+package me.t3sl4.gelkurye.Screens.General;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.t3sl4.gelkurye.R;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
