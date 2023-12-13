ac39cb8c4554c20ee913b6624a0c17c3085e7969
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-12-13 22:43:21 +0300

    Theme Update

--- app/src/main/java/me/t3sl4/gelkurye/Screens/MainActivity.java
+++ app/src/main/java/me/t3sl4/gelkurye/Screens/MainActivity.java
@@ -1,15 +1,35 @@
 package me.t3sl4.gelkurye.Screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import me.t3sl4.gelkurye.R;
+import me.t3sl4.gelkurye.Screens.Authentication.Login;
+import me.t3sl4.gelkurye.Screens.Authentication.Register;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+        });
+
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
