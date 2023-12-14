c9014fc7662ac6e37c92a8c7f1d8b889c26b5179
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-12-14 17:21:04 +0300

    Main Screen Update

--- app/src/main/java/me/t3sl4/gelkurye/Screens/Authentication/Login.java
+++ app/src/main/java/me/t3sl4/gelkurye/Screens/Authentication/Login.java
@@ -1,12 +1,17 @@
 package me.t3sl4.gelkurye.Screens.Authentication;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import me.t3sl4.gelkurye.R;
+import me.t3sl4.gelkurye.Screens.General.Welcome;
+import me.t3sl4.gelkurye.Screens.MainActivity;
 import me.t3sl4.gelkurye.Util.Component.PasswordFieldTouchListener;
 
 public class Login extends AppCompatActivity  {
@@ -21,7 +26,12 @@ public class Login extends AppCompatActivity  {
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
+        Button loginButton = findViewById(R.id.loginButton);
 
         PasswordFieldTouchListener.setChangeablePasswordField(editTextPassword, getApplicationContext());
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, Welcome.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/me/t3sl4/gelkurye/Screens/General/Welcome.java
+++ app/src/main/java/me/t3sl4/gelkurye/Screens/General/Welcome.java
@@ -1,16 +1,70 @@
 package me.t3sl4.gelkurye.Screens.General;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import me.t3sl4.gelkurye.R;
 
 public class Welcome extends AppCompatActivity {
+    private NavigationView hamburgerMenu;
+    private ImageView navigationButton;
+    private ConstraintLayout mainLayout;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        hamburgerMenu = findViewById(R.id.hamburgerMenu);
+        navigationButton = findViewById(R.id.navigationButton);
+        mainLayout = findViewById(R.id.mainLayout);
+
+        navigationButton.setOnClickListener(v -> {
+            showNavigationViewWithAnimation();
+        });
+
+        mainLayout.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN && hamburgerMenu.getVisibility() == View.VISIBLE) {
+                hideNavigationViewWithAnimation();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void showNavigationViewWithAnimation() {
+        Animation slideIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        hamburgerMenu.setVisibility(View.VISIBLE);
+        hamburgerMenu.startAnimation(slideIn);
+    }
+
+    private void hideNavigationViewWithAnimation() {
+        Animation slideOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_right);
+        slideOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                hamburgerMenu.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        hamburgerMenu.startAnimation(slideOut);
     }
 }
