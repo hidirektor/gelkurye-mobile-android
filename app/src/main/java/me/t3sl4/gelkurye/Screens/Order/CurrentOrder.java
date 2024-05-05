ccf90b522bd1fcea35c522e2fac6cc64a439ad84
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-05-05 14:11:42 +0300

    Google Maps
    Signed-off-by: Halil İbrahim Direktör <hidirektor@gmail.com>

--- app/src/main/java/me/t3sl4/gelkurye/Screens/Order/CurrentOrder.java
+++ app/src/main/java/me/t3sl4/gelkurye/Screens/Order/CurrentOrder.java
@@ -1,94 +1,226 @@
 package me.t3sl4.gelkurye.Screens.Order;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.maps.DirectionsApi;
-import com.google.maps.DirectionsApiRequest;
 import com.google.maps.GeoApiContext;
-import com.google.maps.PendingResult;
+import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
 import com.google.maps.model.TravelMode;
+import com.google.maps.model.Unit;
+import com.google.type.DateTime;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import me.t3sl4.gelkurye.R;
 
-public class CurrentOrder extends AppCompatActivity implements OnMapReadyCallback {
+public class CurrentOrder extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
+    private static final int overview = 0;
+    private final int PLACE_AUTOCOMPLETE_REQUEST_CODE_FROM = 201;
+    private final int PLACE_AUTOCOMPLETE_REQUEST_CODE_TO = 202;
+    private String TAG = getClass().getSimpleName();
     private GoogleMap mMap;
-    private LatLng deviceLocation = new LatLng(38.49812851901897, 27.706092943097573);
-    private LatLng targetLocation = new LatLng(38.491881428107426, 27.705858825909353);
+    private TextView mTvFrom;
+    private TextView mTvTo;
+    //private String GOOGLE_PLACES_API_KEY = "YOUR GOOGLE PLACES API KEY";
+
+    private List<Marker> markers;
+    private Polyline polyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_current);
+        bindActivity();
+    }
 
+    private void bindActivity() {
+        mTvFrom = findViewById(R.id.tv_from);
+        mTvTo = findViewById(R.id.tv_to);
+
+        findViewById(R.id.get_directions).setOnClickListener(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mTvFrom.setOnClickListener(this);
+        mTvTo.setOnClickListener(this);
+    }
+
+    private void setupGoogleMapScreenSettings(GoogleMap mMap) {
+        mMap.setBuildingsEnabled(true);
+        UiSettings mUiSettings = mMap.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setCompassEnabled(true);
+        mUiSettings.setMyLocationButtonEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
+        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setRotateGesturesEnabled(true);
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        Bitmap deviceIcon = BitmapFactory.decodeResource(getResources(), R.drawable.navbar_home_ikon);
-        if (deviceIcon != null) {
-            mMap.addMarker(new MarkerOptions()
-                    .position(deviceLocation)
-                    .title("Hedef Konum")
-                    .icon(BitmapDescriptorFactory.fromBitmap(deviceIcon)));
-        }
-
-        Bitmap targetIcon = BitmapFactory.decodeResource(getResources(), R.drawable.logo_main);
-        if (targetIcon != null) {
-            mMap.addMarker(new MarkerOptions()
-                    .position(targetLocation)
-                    .title("Cihaz Konumu")
-                    .icon(BitmapDescriptorFactory.fromBitmap(targetIcon)));
-        }
-
-        mMap.setOnCameraIdleListener(() -> mMap.animateCamera(CameraUpdateFactory.zoomTo(mMap.getCameraPosition().zoom)));
-
-        GeoApiContext context = new GeoApiContext.Builder()
-                .apiKey("AIzaSyBwOPs2oc0XZfNc3ndeZmZpiL3BaH2R4pk")
-                .build();
-        DirectionsApiRequest request = DirectionsApi.getDirections(context, deviceLocation.latitude + "," + deviceLocation.longitude,
-                        targetLocation.latitude + "," + targetLocation.longitude)
-                .mode(TravelMode.DRIVING);
-        request.setCallback(new PendingResult.Callback<DirectionsResult>() {
-            @Override
-            public void onResult(DirectionsResult result) {
-                if (result.routes != null && result.routes.length > 0) {
-                    PolylineOptions polylineOptions = new PolylineOptions();
-                    for (com.google.maps.model.LatLng point : result.routes[0].overviewPolyline.decodePath()) {
-                        polylineOptions.add(new LatLng(point.lat, point.lng));
-                    }
-                    Polyline polyline = mMap.addPolyline(polylineOptions);
-                    polyline.setColor(Color.rgb(255, 165, 0));
-                }
-            }
-
-            @Override
-            public void onFailure(Throwable e) {
-                runOnUiThread(() -> Toast.makeText(CurrentOrder.this, "Yol tarifi alınamadı.", Toast.LENGTH_SHORT).show());
-            }
-        });
+        setupGoogleMapScreenSettings(googleMap);
+        LatLng latLng = new LatLng(13.060530, 80.242329);
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Nungambakkam, Chennai, Tamil Nadu"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    /*@Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.tv_from) {
+            pickLocation(PLACE_AUTOCOMPLETE_REQUEST_CODE_FROM);
+        } else if(view.getId() == R.id.tv_to) {
+            pickLocation(PLACE_AUTOCOMPLETE_REQUEST_CODE_TO);
+        } else if(view.getId() == R.id.get_directions) {
+            if (mTvFrom.length() <= 0) {
+                showMessage("Please pick from address");
+                return;
+            }
+            if (mTvTo.length() <= 0) {
+                showMessage("Please pick to address");
+                return;
+            }
+            getResult();
+        }
+    }
+
+    private void getResult() {
+        DirectionsResult results = getDirectionsDetails(mTvFrom.getText().toString(), mTvTo.getText().toString(), TravelMode.DRIVING);
+        if (results != null) {
+            addPolyline(results);
+            addMarkersToMap(results);
+            positionCamera(results.routes[overview]);
+        }
+    }
+
+    private void addMarkersToMap(DirectionsResult results) {
+        Marker markerSrc = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(results.routes[overview].legs[overview].startLocation.lat, results.routes[overview].legs[overview].startLocation.lng))
+                .title(results.routes[overview].legs[overview].startAddress));
+        Marker markerDes = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(results.routes[overview].legs[overview].endLocation.lat, results.routes[overview].legs[overview].endLocation.lng))
+                .title(results.routes[overview].legs[overview].endAddress)
+                .snippet(getEndLocationTitle(results)));
+        markers = new ArrayList<>();
+        markers.add(markerSrc);
+        markers.add(markerDes);
+    }
+
+    private void positionCamera(DirectionsRoute route) {
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker marker : markers) {
+            builder.include(marker.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 150);
+        mMap.animateCamera(cu);
+    }*/
+
+    /*private void addPolyline(DirectionsResult results) {
+        if (polyline != null) {
+            polyline.remove();
+        }
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[overview].overviewPolyline.getEncodedPath());
+        polyline = mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
+    }
+
+    private String getEndLocationTitle(DirectionsResult results) {
+        return "Time :" + results.routes[overview].legs[overview].duration.humanReadable + " Distance :" + results.routes[overview].legs[overview].distance.humanReadable;
+    }
+
+    private GeoApiContext.Builder getGeoContext() {
+        return new GeoApiContext.Builder()
+                .apiKey(getString(R.string.google_maps_key))
+                .queryRateLimit(3)
+                .queryRateLimitPeriod(1, TimeUnit.SECONDS)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS);
+    }
+
+    private DirectionsResult getDirectionsDetails(String origin, String destination, TravelMode mode) {
+        try {
+            return DirectionsApi.newRequest(getGeoContext().build())
+                    .mode(mode)
+                    .origin(origin)
+                    .destination(destination)
+                    .departureTime(new DateTime())
+                    .await();
+        } catch (ApiException | InterruptedException | IOException e) {
+            e.printStackTrace();
+            showMessage(e.getMessage());
+            return null;
+        }
+    }
+
+    private void pickLocation(int requestCode) {
+        try {
+            AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                    .setCountry("IN")
+                    .build();
+
+            Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS))
+                    .setTypeFilter(typeFilter)
+                    .build(this);
+            startActivityForResult(intent, requestCode);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            showMessage(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            Log.i(TAG, "Place: " + place.getName());
+            setResultText(place, requestCode);
+        } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+            Status status = Autocomplete.getStatusFromIntent(data);
+            showMessage(status.getStatusMessage());
+        }
+    }
+
+    private void setResultText(Place place, int requestCode) {
+        //TODO: Implement setResultText method
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }*/
 }
