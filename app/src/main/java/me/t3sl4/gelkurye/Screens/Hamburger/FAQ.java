9d111c6d856377420eab600ee65e8aca87649555
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-05-07 00:24:59 +0300

    F.A.Q. Update
    Signed-off-by: Halil İbrahim Direktör <hidirektor@gmail.com>

--- app/src/main/java/me/t3sl4/gelkurye/Screens/Hamburger/FAQ.java
+++ app/src/main/java/me/t3sl4/gelkurye/Screens/Hamburger/FAQ.java
@@ -15,17 +15,24 @@ import me.t3sl4.gelkurye.R;
 public class FAQ extends AppCompatActivity {
     private ImageView backButton;
     private NavigationView faqList;
+
+
     private LinearLayout question1;
     private LinearLayout answer1;
-
     private LinearLayout question2;
     private LinearLayout answer2;
+    private LinearLayout question3;
+    private LinearLayout answer3;
+    private LinearLayout question4;
+    private LinearLayout answer4;
 
     private Animation slideDownAnimation;
     private Animation slideUpAnimation;
 
     private boolean isFirstAnswerVisible = false;
     private boolean isSecondAnswerVisible = false;
+    private boolean isThirthAnswerVisible = false;
+    private boolean isFourthAnswerVisible = false;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -41,6 +48,10 @@ public class FAQ extends AppCompatActivity {
         answer1 = mainView.findViewById(R.id.answer1);
         question2 = mainView.findViewById(R.id.question2);
         answer2 = mainView.findViewById(R.id.answer2);
+        question3 = mainView.findViewById(R.id.question3);
+        answer3 = mainView.findViewById(R.id.answer3);
+        question4 = mainView.findViewById(R.id.question4);
+        answer4 = mainView.findViewById(R.id.answer4);
 
         slideDownAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
         slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
@@ -48,6 +59,8 @@ public class FAQ extends AppCompatActivity {
         backButton.setOnClickListener(v -> finish());
         question1.setOnClickListener(v -> toggleAnswerVisibility(answer1));
         question2.setOnClickListener(v -> toggleAnswerVisibility(answer2));
+        question3.setOnClickListener(v -> toggleAnswerVisibility(answer3));
+        question4.setOnClickListener(v -> toggleAnswerVisibility(answer4));
     }
 
     private void toggleAnswerVisibility(LinearLayout answerLayout) {
@@ -67,15 +80,41 @@ public class FAQ extends AppCompatActivity {
                 isSecondAnswerVisible = false;
             }
 
+            if(isThirthAnswerVisible) {
+                answer3.startAnimation(slideUpAnimation);
+                answer3.setVisibility(View.GONE);
+                isThirthAnswerVisible = false;
+            }
+
+            if(isFourthAnswerVisible) {
+                answer4.startAnimation(slideUpAnimation);
+                answer4.setVisibility(View.GONE);
+                isFourthAnswerVisible = false;
+            }
+
             answerLayout.startAnimation(slideDownAnimation);
             answerLayout.setVisibility(View.VISIBLE);
 
             if (answerLayout == answer1) {
                 isFirstAnswerVisible = true;
                 isSecondAnswerVisible = false;
+                isThirthAnswerVisible = false;
+                isFourthAnswerVisible = false;
             } else if (answerLayout == answer2) {
                 isSecondAnswerVisible = true;
                 isFirstAnswerVisible = false;
+                isThirthAnswerVisible = false;
+                isFourthAnswerVisible = false;
+            } else if(answerLayout == answer3) {
+                isThirthAnswerVisible = true;
+                isFirstAnswerVisible = false;
+                isSecondAnswerVisible = false;
+                isFourthAnswerVisible = false;
+            } else if(answerLayout == answer4) {
+                isFourthAnswerVisible = true;
+                isFirstAnswerVisible = false;
+                isSecondAnswerVisible = false;
+                isThirthAnswerVisible = false;
             }
         }
     }
