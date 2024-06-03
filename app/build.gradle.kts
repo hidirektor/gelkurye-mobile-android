6dd3047abecc17de00853d1f8369a6bf4d504c6f
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-06-03 09:28:50 +0300

    aab Update
    Signed-off-by: Halil İbrahim Direktör <hidirektor@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "me.t3sl4.kurye"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "03.06.2024"
 
         buildConfigField("String", "BASE_URL", "\"http://85.95.231.92:4000/api/v1/\"")
 
