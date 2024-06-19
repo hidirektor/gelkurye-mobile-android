b31e476c5d6ac69fcbb124ea0ceb6c649907cc73
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-06-19 22:43:08 +0300

    Marketplace Update
    Signed-off-by: Halil İbrahim Direktör <hidirektor@gmail.com>

--- app/src/main/java/me/t3sl4/kurye/UI/Screens/User/Merchant/Marketplace.java
+++ app/src/main/java/me/t3sl4/kurye/UI/Screens/User/Merchant/Marketplace.java
@@ -1,18 +1,38 @@
 package me.t3sl4.kurye.UI.Screens.User.Merchant;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import me.t3sl4.kurye.R;
+import me.t3sl4.kurye.UI.Components.NiceSwitch.NiceSwitch;
 
 public class Marketplace extends AppCompatActivity {
+
+    private NiceSwitch synchTrendyol;
+    private NiceSwitch synchGetirYemek;
+    private NiceSwitch synchYemekSepeti;
+
+    private LinearLayout findTrendyol;
+    private LinearLayout findGetirYemek;
+    private LinearLayout findYemekSepeti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.marketplace);
 
+        componentInitialize();
+    }
 
+    private void componentInitialize() {
+        synchTrendyol = findViewById(R.id.trendyolYemekSwitch);
+        synchGetirYemek = findViewById(R.id.getirYemekSwitch);
+        synchYemekSepeti = findViewById(R.id.yemeksepetiSwitch);
+
+        findTrendyol = findViewById(R.id.findTrendyolButton);
+        findGetirYemek = findViewById(R.id.findGetirYemekButton);
+        findYemekSepeti = findViewById(R.id.findYemekSepetiButton);
     }
 }
